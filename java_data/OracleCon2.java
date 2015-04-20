b9ebac7b6430b3538498e45f9bfe5a24eb96fa92
MasakiFukuda <masaki1014@hotmail.co.jp>
2015-04-20 15:02:15 +0900

    Oracleに接続してDMLを実行するソース

---
+++ java_data/OracleCon2.java
@@ -0,0 +1,33 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class OracleCon2{
+	public static void main(String[] args){
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
+			System.out.println("ê⁄ë±äÆóπ");
+			
+			String sql = "insert into emp(empno,ename) values(1001,'FUKUDA')";
+			
+			Statement st = con.createStatement();
+			
+			int count = st.executeUpdate(sql);
+			
+			System.out.println(count+"åèèàóùäÆóπ");
+			
+			con.commit();
+			
+			st.close();
+			con.close();
+			
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+}
